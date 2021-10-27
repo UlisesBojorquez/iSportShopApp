f34edefe177c12f1f414385d208a1c257dfdb4af
UlisesBojorquez <ulibojorquez@gmail.com>
2021-10-26 23:20:10 -0600

    Drag location on google maps

--- app/src/main/java/com/example/isportshop/MapsActivity.kt
+++ app/src/main/java/com/example/isportshop/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.isportshop
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,38 +19,112 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.isportshop.databinding.ActivityMapsBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var lastLocation : Location
+    //private lateinit var fusedLocationClient : FusedLocationProviderClient
+    var fusedLocationProviderClient : FusedLocationProviderClient? = null
+    var currentLocation : Location? = null
+    var currentMarker : Marker? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
+        /*val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
+        mapFragment.getMapAsync(this)*/
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    }
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latLong = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
+        drawMarker(latLong)
+
+        mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener{
+            override fun onMarkerDrag(p0: Marker) {
+
+            }
+
+            override fun onMarkerDragEnd(p0: Marker) {
+                if(currentMarker !=null)
+                    currentMarker?.remove()
+
+                val newLatLng = LatLng(p0?.position!!.latitude, p0?.position!!.longitude)
+                drawMarker(newLatLng)
+            }
+
+            override fun onMarkerDragStart(p0: Marker) {
+
+            }
+        })
     }
+
+    private fun drawMarker(latLong : LatLng){
+        val markerOption = MarkerOptions().position(latLong).title(latLong.toString())
+            .snippet(getAddress(latLong.latitude, latLong.longitude)).draggable(true)
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLong))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLong, 15f),4000, null)
+        currentMarker = mMap.addMarker(markerOption)
+        currentMarker?.showInfoWindow()
+
+    }
+
+    private fun getAddress(latitude: Double, longitude: Double): String? {
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        val address = geoCoder.getFromLocation(latitude, longitude, 1)
+        return address[0].getAddressLine(0).toString()
+
+    }
+
+    private fun fetchLocation() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1000)
+            return
+        }
+        val lastLocation = fusedLocationProviderClient?.lastLocation
+        lastLocation?.addOnSuccessListener { location ->
+            if(location != null){
+                this.currentLocation = location
+                val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            1000 -> if(grantResults.size>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                fetchLocation()
+        }
+    }
+
+
+
+
 }
