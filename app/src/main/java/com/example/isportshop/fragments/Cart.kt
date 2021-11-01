07f6dd987c2f86f2f33ebbebf5aa4a5be68dd9c8
LindaNayeli104 <linda_nayeli104@hotmail.com>
2021-11-01 17:00:17 -0600

    fix: solving db issue 2

--- app/src/main/java/com/example/isportshop/fragments/Cart.kt
+++ app/src/main/java/com/example/isportshop/fragments/Cart.kt
@@ -117,7 +117,11 @@ class Cart : Fragment() {
                         var nameDocument = document["name"].toString()
                         for (item in this.itemsList) {
                             //Log.d("entroooooooooo", item)
+<<<<<<< Updated upstream
                             if (nameDocument.equals(item.key)) {  //Si no funciona, cambiar aqui/
+=======
+                            if (nameDocument.equals(item.key)) {  /Si no funciona, cambiar aqui/
+>>>>>>> Stashed changes
                                 listProduct.add(
                                     ProductCart(
                                         document["name"].toString(),
